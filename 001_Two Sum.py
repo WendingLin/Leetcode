6db3d1fb9a774306fe55d5c1f6d6d1ce2db80a61
Lycoris <lycoris000@126.com>
2017-01-30 19:45:57 +0800

    Add Problem 1

---
+++ 001_Two Sum.py
@@ -0,0 +1,38 @@
+#1.Two Sum
+class Solution(object):
+    def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #ON2
+        """
+        loca_a=0
+        loca_b=1
+        flag=0
+        while loca_a<len(nums):
+            loca_b=loca_a+1
+            while loca_b<len(nums) :
+                if nums[loca_a]+nums[loca_b]==target:
+                    flag=1
+                    break
+                else:
+                    loca_b+=1
+            if flag==1:
+                break
+            else:
+                loca_a+=1
+        return [loca_a,loca_b]
+        """
+        #ON
+        dict={}
+        for loca in range(len(nums)):
+            if nums[loca] in dict:
+                return [dict[nums[loca]], loca]
+            else:
+                dict[target-nums[loca]]=loca
+    nums=[2,7,11,15]
+    target=9
+    print(twoSum(nums,target))
+
