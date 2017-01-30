4fc37c18282e196bf9fe30eb3e52ee45650072f4
Lycoris <lycoris000@126.com>
2017-01-30 17:58:55 +0800

    Add Problem 31

--- 031_Next Permutation.py
+++ 031_Next Permutation.py
@@ -5,3 +5,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+    #Steps:
+    #1. Find two successive ascending elements from the trailer
+    #2. Mark the first element as 'Partition'
+    #3. Find another element larger than the 'Partition' element
+    #4. Change the location of them
+    #5. Sort the elements after the 'Partition' location in the reversed order
+        loca_part=len(nums)-2
+        while loca_part>-1:
+            if nums[loca_part]<nums[loca_part+1]:
+                break
+            else:
+                loca_part-=1
+        if loca_part==-1:
+            nums.reverse()
+        else:
+            for loca_change in range(len(nums)-1, loca_part, -1):
+                if nums[loca_change]>nums[loca_part]:
+                    nums[loca_change]+=nums[loca_part]
+                    nums[loca_part]=nums[loca_change]-nums[loca_part]
+                    nums[loca_change]=nums[loca_change]-nums[loca_part]
+                    break
+            nums[loca_part+1:]=list(reversed(nums[loca_part+1:]))
+s=Solution()
+nums=[1, 2, 3]
+s.nextPermutation(nums)
+print(nums)
