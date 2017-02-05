48ba65105e2a9048b45511b2d78f3bdc12eac5c7
Lycoris <lycoris000@126.com>
2017-02-05 23:05:36 +0800

    Add Solution 219

--- 219_Contains Duplicate II.py
+++ 219_Contains Duplicate II.py
@@ -6,4 +6,19 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-    #Sort
+    #My Solution
+        # dict={}
+        # for i in range(len(nums)):
+        #     if nums[i] in dict and abs(dict[nums[i]]-i)<=k: return True
+        #     else: dict[nums[i]]=i
+        # return False
+    #My Solution(Set)
+        window=set([])
+        for i in range(len(nums)):
+            if i>k:
+                window.discard(nums[i-k-1])
+            if nums[i] in window:
+                return True
+            else:
+                window.add(nums[i])
+        return False
